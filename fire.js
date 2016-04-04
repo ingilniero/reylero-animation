4d732a13680d7e1fd651257f0b6bfea823ccf961
Gilberto Villa <ingilniero@gmail.com>
2016-04-03 23:58:32 -0500

    WIP

--- fire.js
+++ fire.js
@@ -3,49 +3,78 @@
 */
 
 var fire;
+var mosquito;
 
-function launch()
-{
+function launch() {
+  //2000 × 571
   fire = sprite({
-    w       : 1999,
+    w       : 2000,
     h       : 550,
     ctx     : fireCanvas.getContext('2d'),
     img     : fireImage,
     numberOfFrames: 7,
-    ticksPerFrame: 3
+    ticksPerFrame: 3,
+    scaleRatio : .5,
+    yOffset    : 0
   });
-  fireLoop();
+
+  //5381 × 1012
+  mosquito = sprite({
+    w              : 5380,
+    h              : 1000,
+    ctx            : mosquitoCanvas.getContext('2d'),
+    img            : mosquitoImage,
+    numberOfFrames : 8,
+    ticksPerFrame  : 5,
+    scaleRatio     : 1,
+    yOffset        : 450
+  });
+
+  animationLoop();
 }
 
-function sprite(o)
-{
+function sprite(o) {
   var that = {};
 
   that.context = o.ctx;
   that.width = o.w;
   that.height = o.h;
   that.image = o.img;
-  that.scaleRatio = .5;
+  that.scaleRatio = o.scaleRatio;
+  that.offset = o.yOffset;
+
   var frameIndex = 0,
     tickCount = 0,
     ticksPerFrame = o.ticksPerFrame || 0,
     numberOfFrames = o.numberOfFrames || 1;
 
   that.render = function(){
-    that.context.clearRect(0, 0, that.width, that.height);
+    console.log('render');
+    //that.context.clearRect(0, 0, that.width, that.height);
     //draw animation
+    var img = that.image;
+    var sx = frameIndex * that.width / numberOfFrames;
+    var sy = that.offset;
+    var swidth = that.width / numberOfFrames;
+    var sheight = that.height;
+    var cx = 0;
+    var cy = 0;
+    var imgWidth = that.width / numberOfFrames * that.scaleRatio;
+    var imgHeight = that.height * that.scaleRatio;
     that.context.drawImage(
-      that.image,
-      frameIndex * that.width / numberOfFrames,
-      0,
-      that.width / numberOfFrames,
-      that.height,
-      0,
-      0,
-      that.width / numberOfFrames * that.scaleRatio,
-      that.height * that.scaleRatio);
+      img,
+      sx,
+      sy,
+      swidth,
+      sheight,
+      cx,
+      cy,
+      imgWidth,
+      imgHeight);
   };
+
   that.update = function(){
+    console.log('update');
     tickCount += 1;
 
     if (tickCount > ticksPerFrame) {
@@ -53,9 +82,9 @@ function sprite(o)
       tickCount = 0;
 
       // If the current frame index is in range
-      if (frameIndex < numberOfFrames - 1) {  
+      if (frameIndex < numberOfFrames - 1) {
         // Go to the next frame
-        frameIndex += 1; 
+        frameIndex += 1;
       } else {
         frameIndex = 0;
       }
@@ -64,14 +93,20 @@ function sprite(o)
   return that;
 }
 
-function fireLoop()
-{
-  window.requestAnimationFrame(fireLoop);
+function animationLoop() {
+  window.requestAnimationFrame(animationLoop);
+
   fire.update();
   fire.render();
-  console.log('you spin me right round baby right round');
-  follow();
+
+  mosquito.update();
+  mosquito.render();
+
+  //follow();
 }
+
+var mosquitoImage = new Image();
+mosquitoImage.src = 'mosquito.png';
 
 var fireImage = new Image();
 // Load sprite sheet
@@ -79,30 +114,32 @@ fireImage.addEventListener("load", launch);
 fireImage.src = "fire4.png";
 
 var fireCanvas = document.getElementById('fireCanvas');
-fireCanvas.width = 150;
-fireCanvas.height = 265;
-$(fireCanvas).offset({top: -1, left: -1});
+fireCanvas.width = 2000;
+fireCanvas.height = 300;
 
-function follow()
-{
-  $(fireCanvas).offset({
+var mosquitoCanvas = document.getElementById('mosquitoCanvas');
+mosquitoCanvas.width = 5380;
+mosquitoCanvas.height = 200;
+
+$(mosquitoCanvas).offset({top: -1, left: -1});
+
+function follow() {
+  $(mosquitoCanvas).offset({
     top: getTop(),
     left: getLeft()
   });
-
 }
 
-function getTop()
-{
+function getTop() {
   return (currentMousePos.y - ($(fireCanvas).height() / 2));
 }
 
-function getLeft()
-{
+function getLeft() {
   return (currentMousePos.x - ($(fireCanvas).width() / 2));
 }
 
 var currentMousePos = { x: -1, y: -1 };
+
 $(document).mousemove(function(event) {
   currentMousePos.x = event.pageX;
   currentMousePos.y = event.pageY;
--- reylero.js
+++ reylero.js
@@ -1,6 +1,7 @@
-var reylero = document.getElementById('reylero');
+var reylero = document.getElementById('reylero-arm');
 var ctx = reylero.getContext('2d');
-var mouse = { x: -1, y: -1 };
+var mouse = { x: 0, y: 0 };
+var rotation = -0.3
 
 $(document).mousemove(function(evt) {
 
@@ -10,35 +11,51 @@ $(document).mousemove(function(evt) {
   mouse.y = m.y;
 });
 
+var handImg = new Image();
+var armImg = new Image();
+
+handImg.src = 'img/hand-1.png'
+armImg.src = 'img/arm.png';
+
+//579 × 719
+// -170, -280
 var arm = {
-  x: 200,
-  y: 600,
-  width: 500,
-  height: 100
+  x: -900,
+  y: -700,
+  width: 579,
+  height: 719,
+  offsetX: -170,
+  offsetY: -280
 };
+
 
 function armLoop() {
   window.requestAnimationFrame(armLoop);
 
-  var targetX = mouse.x - arm.x;
-  var targetY = mouse.y - arm.y;
+  var targetX = mouse.x - (reylero.width/2);
+  var targetY = mouse.y - (reylero.height/2);
 
-  var rotation = Math.atan2(targetY, targetX);
+  var newRotation = Math.atan2(targetY, targetX) + 2;
+
+
+  if(Math.abs(newRotation) > 0.1 && Math.abs(newRotation) < 0.4)
+    rotation = newRotation;
 
   ctx.clearRect(0, 0, reylero.width, reylero.height);
 
   ctx.save();
 
-  ctx.translate(arm.x + arm.width/2, arm.y + arm.height/2);
+  ctx.translate(reylero.width/2, reylero.height/2);
 
   ctx.rotate(rotation);
 
   ctx.fillStyle = '#008BCC';
+  ctx.fillRect(19/2 * -1, 7/2 * -1, 19, 7);
 
-  ctx.fillRect(arm.width/2 * -1,
-               arm.height/2 * -1,
-               arm.width,
-               arm.height);
+  ctx.drawImage(armImg,arm.offsetX - 115, arm.offsetY - 145 );
+
+  ctx.drawImage(handImg, arm.offsetX - 115, arm.offsetY - 145 );
+
   ctx.restore();
 }
 
